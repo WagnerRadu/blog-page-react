9c463818a3052dcd38364f1b37ea49dc598a353d
Wagner Radu <raduwagner0@gmail.com>
2022-05-18 17:20:17 +0300

    Modal toggle on add/edit buttons + working delete

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ArticleModel } from '../pages/Home';
 import "./style.css"
 
 type Props = {
@@ -9,9 +10,11 @@ type Props = {
   imgUrl: string;
   content: string;
   id: number;
+  editArticle: (article: ArticleModel) => void;
+  deleteArticle: (id: number) => void;
 }
 
-export default function Article({title, tag, author, date, imgUrl, content, id}: Props) {
+export default function Article({title, tag, author, date, imgUrl, content, id, editArticle, deleteArticle}: Props) {
   return (
     <article className='article'>
       <h1 className='title'>{title}</h1>
@@ -21,9 +24,9 @@ export default function Article({title, tag, author, date, imgUrl, content, id}:
         <li className='details-item'>{date}</li>
       </ul>
       <div className='edit-container'>
-        <button className='edit-button'>Edit</button>
+        <button className='edit-button' onClick={() => editArticle({title, tag, author, date, imgUrl, content, id})}>Edit</button>
         <p className='edit-button'>|</p>
-        <button className='edit-button'>Delete</button>
+        <button className='edit-button' onClick={() => deleteArticle(id)}>Delete</button>
       </div>
       <img className='image' src={imgUrl} alt=""/>
       <p className='text'>{content}</p>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
+import AddBtn from '../components/AddBtn';
 import Article from '../components/Article';
+import Modal from '../components/Modal';
+import NavBar from '../components/NavBar';
 
 type Props = {}
 
 type State = {
-    articles: ArticleModel[];
+  articles: ArticleModel[];
+  isModalOpen: boolean;
+  selectedArticle: ArticleModel;
 }
 
-type ArticleModel = {
+export type ArticleModel = {
     title: string;
     tag: string;
     author: string;
@@ -21,8 +26,22 @@ class Home extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-        articles: []
+        articles: [],
+        isModalOpen: false,
+        selectedArticle: {
+          title: "",
+          tag: "",
+          author: "",
+          date: "",
+          imgUrl: "",
+          content: "",
+          id: 0
+        }
     };
+    this.closeModal = this.closeModal.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.deleteArticle = this.deleteArticle.bind(this);
+    this.editArticle = this.editArticle.bind(this);
   }
 
   async componentDidMount() {
@@ -36,8 +55,45 @@ class Home extends Component<Props, State> {
     this.setState({ articles: json });
   }
 
+  closeModal() {
+    this.setState({
+      isModalOpen: false,
+      selectedArticle: {
+        title: "",
+        tag: "",
+        author: "",
+        date: "",
+        imgUrl: "",
+        content: "",
+        id: 0
+      }
+    });
+  }
+
+  openModal() {
+    this.setState({ isModalOpen: true });
+  }
+
+  editArticle(article: ArticleModel) {
+    console.log(article);
+    
+    this.setState({ selectedArticle: article, isModalOpen: true });
+  }
+
+  async deleteArticle(id: number) {
+    const response = await fetch(`http://localhost:3000/articles/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
+
+    this.fetchArticles();
+  }
+
   render() {
-    const { articles } = this.state;
+    const { isModalOpen, articles, selectedArticle } = this.state;
     const articleList = articles.map((article: ArticleModel) => (
         <Article
             key={article.id}
@@ -48,11 +104,18 @@ class Home extends Component<Props, State> {
             imgUrl={article.imgUrl} 
             content={article.content} 
             id={article.id} 
+            deleteArticle={this.deleteArticle}
+            editArticle={this.editArticle}
         />
       ));
 
     return (
-      <div className='container'>{articleList}</div>
+      <div className='container'>
+        <NavBar/>
+        <AddBtn openModal={this.openModal}/>
+        {articleList}
+        <Modal isModalOpen={isModalOpen} article={selectedArticle} closeModal={this.closeModal} ></Modal>
+      </div>
     )
   }
 }
